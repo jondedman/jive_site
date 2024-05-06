60ab0158efb002b8c5db85f1ff2945117cbe01b5
Jon Dedman <jonathandedman@googlemail.com>
2024-05-06 13:52:04 +0100

    update publications header

--- src/Publication_list.jsx
+++ src/Publication_list.jsx
@@ -2,7 +2,8 @@ function Publication_list() {
 	return (
 		<section id="publications" className="py-24">
 			<h2 className="font-manrope text-4xl font-bold text-gray-900 text-center px-6 mb-8">
-				Publications
+				<span className="text-cyan-500 font-bold">P</span>
+				ublications
 			</h2>
 			<div className="flex mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
 				<ul className="space-y-4 leading-8 px-4 text-left">
